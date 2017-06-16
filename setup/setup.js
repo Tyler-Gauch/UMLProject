e3e67019788fbe53730cdb632d9bc6001b8a9f27
Tyler Gauch <tyler.gauch@tgauch.net>
2017-06-16 00:01:45 -0400

    Updates to install git hooks

---
+++ setup/setup.js
@@ -0,0 +1,13 @@
+const ncp = require("ncp");
+
+const BASE_DIR = __dirname + "/..";
+const GIT_HOOKS = BASE_DIR + "/git_hooks";
+const GIT_DIR = BASE_DIR + "/.git/hooks";
+
+
+ncp(GIT_HOOKS, GIT_DIR, err => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("Git hooks installed.");
+});
