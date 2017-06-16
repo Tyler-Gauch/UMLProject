89ae49b76b00832af40c168707fd69707a2c88e3
Tyler Gauch <tyler.gauch@tgauch.net>
2017-06-16 00:16:59 -0400

    Removed a little lint for you!

--- helpers/logger.js
+++ helpers/logger.js
@@ -4,26 +4,29 @@ const DEFAULT_LOGGER = "default_logger";
 
 class Logger {
 
-  constructor(props) {
-      this.loggerName = props.loggerName ?: DEFAULT_LOGGER;      
-      winston.level = process.env.LOG_LEVEL;
-  }
-
-  error(message, object) {
-      winston.log("error", message, object);
+    constructor(props) {
+        this.loggerName = props.loggerName;
+        winston.level = process.env.LOG_LEVEL;
     }
-  }
 
-  warning(message, object) {
-    winston.log("warning", message, object);
-  }
+    error(message, object) {
+        winston.log("error", message, object);
+    }
 
-  info(message, object) {
-    winston.log("info", message, object);
-  }
+    warning(message, object) {
+        winston.log("warning", message, object);
+    }
 
-  debug(message, object) {
-    winston.log("debug", message, object);
-  }
+    info(message, object) {
+        winston.log("info", message, object);
+    }
+
+    debug(message, object) {
+        winston.log("debug", message, object);
+    }
 
 }
+
+Logger.loggerName = DEFAULT_LOGGER;
+
+module.exports = Logger;
